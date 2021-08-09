a0e581daa7a10ce38b5641d815211d00080586fc
Bhupendra124 <wadekar.bhupendra6@gmail.com>
2021-08-10 04:05:15 +0530

    ADD:Adding details

--- src/com/Contacts.java
+++ src/com/Contacts.java
@@ -10,7 +10,8 @@ public class Contacts {
     Long phoneNumber;
     String email;
 
-    public Contacts(String firstName, String lastName, String address, String city, String state, Long zipCode, Long phoneNumber, String email) {
+    public Contacts(String firstName, String lastName, String address, String city, String state, Long zipCode,
+                    Long phoneNumber, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -21,23 +22,99 @@ public class Contacts {
         this.email = email;
     }
 
-
-
     @Override
     public String toString() {
-        return "Contacts{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", address='" + address + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", zipCode=" + zipCode +
-                ", phoneNumber=" + phoneNumber +
-                ", email='" + email + '\'' +
-                '}';
+        return "Contacts [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
+                + ", state=" + state + ", zipCode=" + zipCode + ", phoneNumber=" + phoneNumber + ", email=" + email
+                + "]";
     }
 
-    public static void main(String[] args) {
-        Contacts contacts = new Contacts("BHUPENDRA", "WADEKAR", "GOVIND COLONY AFS STATION ", "AMLA", "MP", "460551", "9893902047", "wadekar.bhupendra6@gmail.com");
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(Long zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Contacts))
+            return false;
+
+        Contacts other = (Contacts) obj;
+        if (!(firstName.equalsIgnoreCase(other.getFirstName())))
+            return false;
+        if (!(lastName.equalsIgnoreCase(other.getLastName())))
+            return false;
+        if (!(address.equalsIgnoreCase(other.getAddress())))
+            return false;
+        if (!(city.equalsIgnoreCase(other.getCity())))
+            return false;
+        if (!state.equalsIgnoreCase(other.getState()))
+            return false;
+        if (!zipCode.equals(other.getZipCode()))
+            return false;
+        if (!(phoneNumber.equals(other.getPhoneNumber())))
+            return false;
+        return email.equals(other.getEmail());
     }
 }
